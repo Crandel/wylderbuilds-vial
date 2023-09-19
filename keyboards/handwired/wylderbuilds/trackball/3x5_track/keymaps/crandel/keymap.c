f13706536dc428adbc3c4d4b9a58ca23c6cf8b5c
Vitalii Drevenchuk <4005032+Crandel@users.noreply.github.com>
2023-09-19 15:54:28 +0200

    keymap update

--- keyboards/handwired/wylderbuilds/trackball/3x5_track/keymaps/crandel/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/3x5_track/keymaps/crandel/keymap.c
@@ -55,11 +55,11 @@ void keyboard_post_init_user(void) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [COLMAK_L] = LAYOUT_3x5(
   //,-------------------------------------------------------.    ,---------------------------------------------------------------.
-      KC_Q, RALT_T(KC_W), LCTL_T(KC_F), LSFT_T(KC_P), KC_B,         KC_J, LSFT_T(KC_L), LCTL_T(KC_U), RALT_T(KC_Y),       KC_SCLN,
+      KC_Q, RALT_T(KC_W), LCTL_T(KC_F), LSFT_T(KC_P), KC_B,         KC_E, LSFT_T(KC_O), LCTL_T(KC_U), RALT_T(KC_Y),       KC_SCLN,
   //|-----+-------------+-------------+-------------+-------|    |------+-------------+-------------+-------------+--------------|
-      KC_A,         KC_R,         KC_S,         KC_T, KC_G,         KC_M,         KC_N,         KC_E,         KC_I,          KC_O,
+      KC_A,         KC_R,         KC_S,         KC_T, KC_G,         KC_M,         KC_H,         KC_J,         KC_K,          KC_L,
   //|-----+-------------+-------------+-------------+-------|    |------+-------------+-------------+-------------+--------------|
-      KC_Z,         KC_X,         KC_C,         KC_D, KC_V,         KC_K,         KC_H,      KC_COMM,       KC_DOT,       KC_SLSH,
+      KC_Z,         KC_X,         KC_C,         KC_D, KC_V,         KC_I,         KC_N,      KC_COMM,       KC_DOT,       KC_SLSH,
   //|-----+-------------+-------------+-------------+-------|    |------+-------------+-------------+-------------+--------------|
                                              KC_LGUI, KC_SPC,                   KC_ENT,
                                       // -----------+-------|    |------+-------------|
